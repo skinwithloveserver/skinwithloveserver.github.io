5b0f8598b06a424fd006ebe5915c1c86a1941f08
Gleb R <greznicov@myseneca.ca>
2019-07-29 20:06:44 -0400

    big round of changes

--- services.js
+++ services.js
@@ -16,6 +16,60 @@ window.onload = () => {
         navSmallExpanded = navSmallExpanded ? false : true;
         expandNavSmall(navSmallExpanded);
     };
+
+    document.querySelector(".buttonPopup1").onclick = () => {
+        Swal.fire({
+            imageUrl: "./images/instagram.png",
+            imageWidth: 80,
+            imageHeight: 80,
+            title: 'Book Appointment',
+            text: 'Head on over to Instagram to follow @skinwithlovestudio to book your appointment!',
+            showCancelButton: true,
+            cancelButtonText: "Cancel",
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Book Now!'
+          }).then((result) => {
+              if(result.value) {
+                  window.location.href = "https://www.instagram.com/skinwithlovestudio/?utm_source=ig_profile_share&igshid=1u69eaiosxy0x";
+              }
+          });
+    }
+
+    document.querySelector(".buttonPopup2").onclick = () => {
+        Swal.fire({
+            imageUrl: "./images/instagram.png",
+            imageWidth: 80,
+            imageHeight: 80,
+            title: 'Book Appointment',
+            text: 'Head on over to Instagram to follow @skinwithlovestudio to book your appointment!',
+            showCancelButton: true,
+            cancelButtonText: "Cancel",
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Book Now!'
+          }).then((result) => {
+              if(result.value) {
+                  window.location.href = "https://www.instagram.com/skinwithlovestudio/?utm_source=ig_profile_share&igshid=1u69eaiosxy0x";
+              }
+          });
+    }
+
+    document.querySelector(".buttonPopup3").onclick = () => {
+        Swal.fire({
+            imageUrl: "./images/instagram.png",
+            imageWidth: 80,
+            imageHeight: 80,
+            title: 'Book Appointment',
+            text: 'Head on over to Instagram to follow @skinwithlovestudio to book your appointment!',
+            showCancelButton: true,
+            cancelButtonText: "Cancel",
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Book Now!'
+          }).then((result) => {
+              if(result.value) {
+                  window.location.href = "https://www.instagram.com/skinwithlovestudio/?utm_source=ig_profile_share&igshid=1u69eaiosxy0x";
+              }
+          });
+    }
 };
 
 window.addEventListener(
